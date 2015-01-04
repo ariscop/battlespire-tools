26d44a61167bf290151d1ef165bbf2aebb0eb4b2
Andrew Cook <ariscop@gmail.com>
2015-01-04 19:14:05 +1100

    Further changes to bsitool

--- bsitool/bsitool.py
+++ bsitool/bsitool.py
@@ -7,6 +7,12 @@ from struct import unpack, unpack_from, iter_unpack
 from pprint import pprint
 from collections import defaultdict, namedtuple
 
+def unpack_15bpp(pixel):
+    return (
+        (8 * (pixel & 0b0111110000000000) >> 10),
+        (8 * (pixel & 0b0000001111100000) >> 5),
+        (8 * (pixel & 0b0000000000011111) >> 0)
+    )
 
 class DataElement(ET.Element):
     def __init__(self, tag, attrib={}):
@@ -47,12 +53,12 @@ def parse_ifhd(tree, node, data):
     node.text = to_hex(data.rstrip(b'\x00'))
 
 def parse_bhdr(tree, node, data):
-    u0, u1, width, height, u2, frames, u3, u4, u5, u6, flags = unpack("<4H6sH5H", data)
+    u0, u1, width, height, u2, frames, u3, u4, u5, u6, flags = unpack("<4H6s6H", data)
     node.append(element("width", text=width))
     node.append(element("height", text=height))
     node.append(element("frames", text=frames))
     node.append(element("u0", text=u0)) #corolation with width,
-    node.append(element("u1", text=u1)) #height. aspect ratio?
+    node.append(element("u1", text=u1)) #some kind of offset
     node.append(element("u2", text=to_hex(u2))) #01, 02, or 03
     node.append(element("u3", text=u3)) #values; 0 71 171 157 100 85 214 128 42 114 142 257 200 57 228
     node.append(element("u4", text=u4)) #0 or 2
@@ -62,11 +68,29 @@ def parse_bhdr(tree, node, data):
 
 def parse_cmap(tree, node, data):
     i = -1
-    for r, g, b in iter_unpack("3B", data):
+    for g, b, r in iter_unpack("3B", data):
         i += 1
         if r == g == b == 0:
             continue
         node.append(element("colour", text="%d %d %d" % (r, g, b), attrib={"id": str(i)}))
+
+def parse_hicl(tree, node, data):
+    i = -1
+    for x in iter_unpack(">H", data):
+        i += 1
+        r, g, b = unpack_15bpp(x[0])
+        if r == g == b == 0:
+            continue
+        node.append(element("colour", text="%d %d %d" % (r, g, b), attrib={"id": str(i)}))
+
+def parse_htbl(tree, node, data):
+    i = -1
+    res = []
+    for x in iter_unpack("256s", data):
+        i += 1
+        node.append(element("unknown", text=to_hex(x[0]), attrib={"id": str(i)}))
+        res.append(x[0])
+    node.data = res
 
 def parse_unknown(tree, node, data):
     sys.stderr.write("Unknown block type: %s\n" % node.tag)
@@ -81,8 +105,8 @@ parsers.update({
     "BHDR": parse_bhdr, #26 bytes fixed
     "NAME": parse_string, #variable
     "DATA": parse_null, #variable
-    "HICL": parse_null, #256 fixed
-    "HTBL": parse_null, #8192 fixed
+    "HICL": parse_hicl, #256 fixed
+    "HTBL": parse_htbl, #8192 fixed
     "CMAP": parse_cmap, #768 fixed #parse_cmap
     "END ": parse_null,
 })
@@ -115,10 +139,14 @@ root = ET.Element("BSIF", {"_length": str(len(data))})
 tree = ET.ElementTree(root)
 readGroup(tree, root, data)
 
-#print(ET.tostring(node))
-print(md.parseString(ET.tostring(root)).toprettyxml())
+xml = md.parseString(ET.tostring(root)).toprettyxml()
+if len(sys.argv) > 3:
+    with open(sys.argv[3], "w") as fd:
+        fd.write(xml)
+else:
+    print(xml)
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     exit()
 
 flags  = int(tree.find(".//BHDR/flags").text)
@@ -127,6 +155,8 @@ height = int(tree.find(".//BHDR/height").text)
 frames = int(tree.find(".//BHDR/frames").text)
 cmap = tree.find(".//CMAP").data
 data = tree.find(".//DATA").data
+hicl = tree.find(".//HICL").data
+htbl = tree.find(".//HTBL")
 
 def decomp(data, width, height):
     out = []
@@ -150,15 +180,58 @@ def decomp(data, width, height):
             [out.append(x) for x in data[offset:offset+width]]
         else:
             raise Exception("Unknown compression %s", hex(comp))
-    return(out)
+    return(bytes(out))
 
 
 if flags != 0:
     data = decomp(data, width, height * frames)
 
-palette = [x*4 for x in cmap]
+data = [x & 0x7F for x in data] #how to handle values over 127?
+
+def bpp15_to_rgb(data):
+    ret = []
+    for x in unpack("<128H", data):
+        for y in unpack_15bpp(x):
+            ret.append(y)
+    return ret
+
+palettes = dict()
+
+if htbl:
+    for x in range(0, 32):
+        palettes["htbl-%2d" % x] = bpp15_to_rgb(htbl.data[x])
+
+palettes["hicl"] = bpp15_to_rgb(hicl)
+palettes["cmap"] = [x * 4 for x in cmap]
+
+out = Image.new("P", (width, height * frames))
+out.putdata(data)
+
+for name, palette in palettes.items():
+    out.putpalette(palette)
+    out.save(sys.argv[2] % name)
+
+exit(0)
+
+palette = bpp15_to_rgb(hicl)
+#palette = [x*4 for x in cmap]
+#palette = []
+#for x in iter_unpack("<H", hicl):
+#    x = x[0]
+#    palette.append(4 * (x & 0b0111110000000000) >> 10)
+#    palette.append(4 * (x & 0b0000001111100000) >> 5)
+#    palette.append(4 * (x & 0b0000000000011111) >> 0)
+
+
+#alpha = [0 if x == 0 else 1 for x in data]
 
 out = Image.new("P", (width, height * frames))
 out.putpalette(palette)
 out.putdata(data)
+
+#pil sucks
+#out = out.convert(mode="RGBA")
+#alphalayer = Image.new("1", (width, height * frames))
+#alphalayer.putdata(alpha)
+#out.putalpha(alphalayer)
 out.save(sys.argv[2])
